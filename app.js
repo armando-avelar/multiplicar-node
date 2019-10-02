eaa241c57f9ce69d7b58be215cabe670a22f0fbe
Armando Avelar <pedrooo_1997@hotmail.com>
2019-10-02 16:51:33 -0500

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+const { argv } = require('./config/yargs');
+const colors = require('colors');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+// console.log(process.argv);
+let argv2 = process.argv;
+let parametro = argv[2];
+// let base = parametro.split('=')[1];
+// console.log(argv);
+
+let comando = argv._[0];
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+        console.log('listar');
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${archivo.green}`))
+            .catch(err => console.log(err));
+    default:
+        console.log('comando no reconocido');
+        break;
+}
+
